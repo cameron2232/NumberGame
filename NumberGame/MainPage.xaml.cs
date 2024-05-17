a75f98839df998744ad72f73a33ae9310b22004f
Cameron <mcleancam22@gmail.com>
2024-05-17 13:56:28 -0400

    Second Commit
    Generated Result of Game

--- NumberGame/MainPage.xaml.cs
+++ NumberGame/MainPage.xaml.cs
@@ -2,13 +2,41 @@
 
 public partial class MainPage : ContentPage
 {
-	
+    int systemNumber;
+    int userNumber;
+    Random randomGen;
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+        // creates an instance of Random, same as randomGem = Random() in python
+        randomGen = new Random();
+        systemNumber = randomGen.Next(100, 1000); // upper bound is excluded
+        userNumber = randomGen.Next(100, 1000);
+    }
 
-	
+    void OnPlay(object sender, EventArgs e)
+    {
+        //DisplayAlert("System Number", $"The number is {systemNumber = GenerateNumber()}", "Ok");
+        //DisplayAlert("User Number", $"The number is {userNumber = GenerateNumber()}", "Ok");
+
+        systemNumber = randomGen.Next(100, 1000); // upper bound is excluded
+        userNumber = randomGen.Next(100, 1000);
+
+        if (userNumber > systemNumber)
+        {
+            DisplayAlert("Result", $"Win! {userNumber} is greater than {systemNumber}", "Ok");
+        }
+        else if (userNumber < systemNumber)
+        {
+            DisplayAlert("Result", $"Lose! {userNumber} is less than {systemNumber}", "Ok");
+        }
+        else
+        {
+            DisplayAlert("Result", $"Jackpot! both numbers are equal to {userNumber}", "Ok");
+        }
+        
+
+    }
 }
 
 
